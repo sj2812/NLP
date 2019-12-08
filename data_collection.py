fcc9fc00259189a04597945fc70447afbbd4d08c
sj2812 <shivani.jadhav2812@gmail.com>
2019-12-08 14:30:56 +0100

    3 array for highlight abstract and other content

--- data_collection.py
+++ data_collection.py
@@ -4,13 +4,34 @@ import os
 # import pandas as pd
 
 tree = ET.parse('articlesinXML/S002243751830416X.xml')
-root = tree.getroot()
-origtext=[]
-#print(root)
 
-# for group in root:
-#     for e in group:
-#         #print(e.tag)
+origtext=[]
+author_highlights=[]
+abst=[]
+
+
+for tag in tree.iter():
+    if(tag.tag == '{http://purl.org/dc/elements/1.1/}description'):
+        # print("Abshhhhhhh")
+        abst.append(tag.text)
+        # print("szfhhhhhhhhhhhhhhhhbj")
+        continue
+    if(tag.tag == '{http://www.elsevier.com/xml/common/dtd}list-item'):
+        continue
+    else:
+        origtext.append(tag.text)
+print(abst)
+
+for tag in tree.iter():
+    if (tag.tag == '{http://www.elsevier.com/xml/common/dtd}list-item'):
+        for t in tag:
+            if(t.tag=='{http://www.elsevier.com/xml/common/dtd}para'):
+                author_highlights.append(t.text)
+print(author_highlights)
+
+print(origtext)
+
+
 '''
 def add_row():
     """
@@ -26,25 +47,16 @@ def add_row():
     para_arr = []
 '''
     # Get abstract
-for coredata in tree.findall('{http://www.elsevier.com/xml/svapi/article/dtd}coredata'):
-    print(coredata.find('{http://purl.org/dc/elements/1.1/}description').text)
+# for coredata in tree.findall('{http://www.elsevier.com/xml/svapi/article/dtd}coredata'):
+#     print(coredata.find('{http://purl.org/dc/elements/1.1/}description').text)
     #coredata_arr.append(coredata.find('{http://purl.org/dc/elements/1.1/}description').text)
 #print(tree.findall('{http://purl.org/dc/elements/1.1/}description'))
 
 
     # get other content
-    print("hbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbb")
-    for tag in tree.iter():
-        if(tag.tag == '{http://purl.org/dc/elements/1.1/}description'):
-            continue
-        if(tag.tag == '{http://www.elsevier.com/xml/common/dtd}abstract-sec'):
-            print("#@$%Abstractttttttttttttttttttt")
-            print(tag.text)
-            continue
-        else:
-            print(tag.text)
 
-    print("gggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggg")
+
+
     # print(origtext)
         # if tag.tag == '{http://www.elsevier.com/xml/svapi/article/dtd}coredata':
         #     continue
@@ -75,105 +87,105 @@ for coredata in tree.findall('{http://www.elsevier.com/xml/svapi/article/dtd}cor
 
 
     # get Author Highlights
-    for originalText in tree.findall('{http://www.elsevier.com/xml/svapi/article/dtd}originalText'):
-        for xcosDoc in originalText.findall('{http://www.elsevier.com/xml/xocs/dtd}doc'):
-            for serialItem in xcosDoc.findall('{http://www.elsevier.com/xml/xocs/dtd}serial-item'):
-                for article in serialItem.findall('{http://www.elsevier.com/xml/ja/dtd}article'):
-                    for head in article.findall('{http://www.elsevier.com/xml/ja/dtd}head'):
-                        for abstract in head.findall('{http://www.elsevier.com/xml/common/dtd}abstract'):
-                            for abstractSec in abstract.findall('{http://www.elsevier.com/xml/common/dtd}abstract-sec'):
-                                for simplePara in abstractSec.findall('{http://www.elsevier.com/xml/common/dtd}simple-para'):
-                                    for listXML in simplePara.findall('{http://www.elsevier.com/xml/common/dtd}list'):
-                                        for listItem in listXML.findall('{http://www.elsevier.com/xml/common/dtd}list-item'):
-                                            for para in listItem.findall('{http://www.elsevier.com/xml/common/dtd}para'):
-                                                print(para.text)
+    # for originalText in tree.findall('{http://www.elsevier.com/xml/svapi/article/dtd}originalText'):
+    #     for xcosDoc in originalText.findall('{http://www.elsevier.com/xml/xocs/dtd}doc'):
+    #         for serialItem in xcosDoc.findall('{http://www.elsevier.com/xml/xocs/dtd}serial-item'):
+    #             for article in serialItem.findall('{http://www.elsevier.com/xml/ja/dtd}article'):
+    #                 for head in article.findall('{http://www.elsevier.com/xml/ja/dtd}head'):
+    #                     for abstract in head.findall('{http://www.elsevier.com/xml/common/dtd}abstract'):
+    #                         for abstractSec in abstract.findall('{http://www.elsevier.com/xml/common/dtd}abstract-sec'):
+    #                             for simplePara in abstractSec.findall('{http://www.elsevier.com/xml/common/dtd}simple-para'):
+    #                                 for listXML in simplePara.findall('{http://www.elsevier.com/xml/common/dtd}list'):
+    #                                     for listItem in listXML.findall('{http://www.elsevier.com/xml/common/dtd}list-item'):
+    #                                         for para in listItem.findall('{http://www.elsevier.com/xml/common/dtd}para'):
+    #                                             print(para.text)
                                                 #para_arr.append(para.text)
                                                 #print(para_arr)
     # write to dataframe
-    """
-    :input : dataframe
-    Creates dataframe
-    :return : Dataframe
-    """
-    '''d = {'Filename': filename, 'Abstract' : coredata_arr, 'Author-Highlights' : para_arr, 'Other Content' : content_arr}
-    data = pd.DataFrame.from_dict(d, orient='index')
-    #print(data)
-    Data_collection_csv = data.to_csv(index=False)
-    print(Data_collection_csv)
-
-"""for filename in glob.glob(os.path.join(path, "*.xml")):
-    with open (filename,"r", encoding="utf-8") as open_file:
-        #print(filename)
-        content = open_file.read()
-        #print(content)
-        #file.write(content)
-        with open(filename, 'r', encoding="utf-8") as input:
-            for index, filename in enumerate(input):
-                with open('filename{}.txt'.format(index), 'w',encoding="utf-8") as output:
-                    output.write(content)"""
-
-
-for file in os.listdir(path):
-    if file.endswith('.xml') or file.endswith('.XML'):
-        file_path = os.path.join(path, file)
-        print(file_path)
-        with open(file_path, encoding="utf-8") as fp:
-            contents = fp.read()
-            print(contents)
-
-
-#Abstract
-"""for originalText in tree.findall('{http://www.elsevier.com/xml/svapi/article/dtd}originalText'):
-    for xcosDoc in originalText.findall('{http://www.elsevier.com/xml/xocs/dtd}doc'):
-        for serialItem in xcosDoc.findall('{http://www.elsevier.com/xml/xocs/dtd}serial-item'):
-            for article in serialItem.findall('{http://www.elsevier.com/xml/ja/dtd}article'):
-                for head in article.findall('{http://www.elsevier.com/xml/ja/dtd}head'):
-                    for abstract in head.findall('{http://www.elsevier.com/xml/common/dtd}abstract'):
-                        for abstractSec in abstract.findall('{http://www.elsevier.com/xml/common/dtd}abstract-sec'):
-                            for simplePara in abstractSec.findall('{http://www.elsevier.com/xml/common/dtd}simple-para'):
-                                for listXML in simplePara.findall('{http://www.elsevier.com/xml/common/dtd}list'):
-                                    for listItem in listXML.findall('{http://www.elsevier.com/xml/common/dtd}list-item'):
-                                        for para in listItem.findall('{http://www.elsevier.com/xml/common/dtd}para'):
-                                            print(para.text)
-"""
-
-'''
-# def create_dataframe():
-#     pass
-#
-# def write_to_dataframe():
 #     """
 #     :input : dataframe
-#     Writes to dataframe
-#     :return : dataframe
+#     Creates dataframe
+#     :return : Dataframe
 #     """
-#     pass
+#     '''d = {'Filename': filename, 'Abstract' : coredata_arr, 'Author-Highlights' : para_arr, 'Other Content' : content_arr}
+#     data = pd.DataFrame.from_dict(d, orient='index')
+#     #print(data)
+#     Data_collection_csv = data.to_csv(index=False)
+#     print(Data_collection_csv)
 #
-# def save_to_csv()
-#     """
-#     :input: dataframe
-#     Saves dataframe to csv
-#     """
-#     pass
+# """for filename in glob.glob(os.path.join(path, "*.xml")):
+#     with open (filename,"r", encoding="utf-8") as open_file:
+#         #print(filename)
+#         content = open_file.read()
+#         #print(content)
+#         #file.write(content)
+#         with open(filename, 'r', encoding="utf-8") as input:
+#             for index, filename in enumerate(input):
+#                 with open('filename{}.txt'.format(index), 'w',encoding="utf-8") as output:
+#                     output.write(content)"""
 #
 #
-# if __name__ == '__main__':
-#     # path = "C:\\Users\\SANKET\\.PyCharmCE2019.3\\config\\scratches"
-#     path = "D:\\TextSumm\\NLP-master\\NLP-master\\articlesinXML"
-#     # "C:\Users\SANKET\.PyCharmCE2019.2\config\scratches"
+# for file in os.listdir(path):
+#     if file.endswith('.xml') or file.endswith('.XML'):
+#         file_path = os.path.join(path, file)
+#         print(file_path)
+#         with open(file_path, encoding="utf-8") as fp:
+#             contents = fp.read()
+#             print(contents)
 #
-#     filenames = glob.glob(os.path.join(path, "*.xml"))
 #
-#     # Create Dataframe
+# #Abstract
+# """for originalText in tree.findall('{http://www.elsevier.com/xml/svapi/article/dtd}originalText'):
+#     for xcosDoc in originalText.findall('{http://www.elsevier.com/xml/xocs/dtd}doc'):
+#         for serialItem in xcosDoc.findall('{http://www.elsevier.com/xml/xocs/dtd}serial-item'):
+#             for article in serialItem.findall('{http://www.elsevier.com/xml/ja/dtd}article'):
+#                 for head in article.findall('{http://www.elsevier.com/xml/ja/dtd}head'):
+#                     for abstract in head.findall('{http://www.elsevier.com/xml/common/dtd}abstract'):
+#                         for abstractSec in abstract.findall('{http://www.elsevier.com/xml/common/dtd}abstract-sec'):
+#                             for simplePara in abstractSec.findall('{http://www.elsevier.com/xml/common/dtd}simple-para'):
+#                                 for listXML in simplePara.findall('{http://www.elsevier.com/xml/common/dtd}list'):
+#                                     for listItem in listXML.findall('{http://www.elsevier.com/xml/common/dtd}list-item'):
+#                                         for para in listItem.findall('{http://www.elsevier.com/xml/common/dtd}para'):
+#                                             print(para.text)
+# """
 #
-#     for filename in filenames:
-#         print(filename)
-#         with open(filename, 'r', encoding="utf-8") as content:
-#             # print(content)
-#             # all_content =content.read()
-#             # print(all_content)
 #
-#             # Call the retrieving function
-#             add_row()
-#
-# '''
+# # def create_dataframe():
+# #     pass
+# #
+# # def write_to_dataframe():
+# #     """
+# #     :input : dataframe
+# #     Writes to dataframe
+# #     :return : dataframe
+# #     """
+# #     pass
+# #
+# # def save_to_csv()
+# #     """
+# #     :input: dataframe
+# #     Saves dataframe to csv
+# #     """
+# #     pass
+# #
+# #
+# # if __name__ == '__main__':
+# #     # path = "C:\\Users\\SANKET\\.PyCharmCE2019.3\\config\\scratches"
+# #     path = "D:\\TextSumm\\NLP-master\\NLP-master\\articlesinXML"
+# #     # "C:\Users\SANKET\.PyCharmCE2019.2\config\scratches"
+# #
+# #     filenames = glob.glob(os.path.join(path, "*.xml"))
+# #
+# #     # Create Dataframe
+# #
+# #     for filename in filenames:
+# #         print(filename)
+# #         with open(filename, 'r', encoding="utf-8") as content:
+# #             # print(content)
+# #             # all_content =content.read()
+# #             # print(all_content)
+# #
+# #             # Call the retrieving function
+# #             add_row()
+# #
+# # '''
